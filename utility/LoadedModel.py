3c4740216bcb628518a188c7c563ed48fc83e8e9
Ariestootl <ariestootle@gmail.com>
2026-09-06 18:54:21 +0800

    updated code in training loop for jupyter notebook

--- utility/LoadedModel.py
+++ utility/LoadedModel.py
@@ -11,11 +11,13 @@ class LoadedModel:
                  activation_dict: dict,
                  model_params: list,
                  device: str,
+                 initial_fileName: str = "chkpt",
                  checkpoint_dir = "model_trained_params"):
         self.activation_dict = activation_dict
         self.model_params = model_params
         self.device = device
         self.checkpoint_dir = checkpoint_dir
+        self.initial_fileName = initial_fileName
         self.load_models = {}
     def load_checkpoints(self):
         for act_name, act_fn in self.activation_dict.items():
@@ -26,7 +28,7 @@ class LoadedModel:
                         output_shape=10, 
                         activation=act_fn, 
                         params=self.model_params).to(self.device)
-            path_to_trained_acn = f"{self.checkpoint_dir}/chkpt_{act_name}.pth"
+            path_to_trained_acn = f"{self.checkpoint_dir}/{self.initial_fileName}_{act_name}.pth"
             if os.path.exists(path_to_trained_acn):
                 trained_model = torch.load(path_to_trained_acn, map_location=self.device, weights_only=False)
                 print(f"Loaded trained model for {act_name} from {path_to_trained_acn}")
